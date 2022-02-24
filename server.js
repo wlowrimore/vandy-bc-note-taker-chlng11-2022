085d215664da4f451cb968aa910592010bffdf70
William Lowrimore <92553989+wlowrimore@users.noreply.github.com>
2022-02-24 13:26:15 -0600

    in fix

--- server.js
+++ server.js
@@ -1,12 +1,55 @@
+const fs = require('fs');
+const path = require('path');
 const { notes } = require('./db/notes');
 const express = require('express');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+notesArray = [];
+
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+
+// parse incoming JSON data
+app.use(express.json());
+
 app.get('/api/notes', (req, res) => {
   res.json(notes);
 });
+
+app.post('/api/notes', (req, res) => {
+  // assign id based on index of array
+  req.body.id = notes.length.toString();
+
+  // if any data in req.body is incorrect, send 400 error back
+  if (!validateNote(req.body)) {
+    res.status(400).send('Your note is not properly formatted.');
+  } else {
+    const note = createNewNote(req.body, notes);
+    res.json(note);
+  }  
+});
+
+function createNewNote(body, notesArray) {
+  const note = body;
+  notesArray.push(note);
+  fs.writeFileSync(
+    path.join(__dirname, './db/notes.json'),
+    JSON.stringify({ notes: notesArray }, null, 2)
+  );  
+  return note;
+}
+
+function validateNote(note) {
+  if (!note.title || typeof note.title !== 'string') {
+    return false;
+  }
+  if (!note.text || typeof note.text !== 'string') {
+    return false;
+  } 
+  return true;
+}
 
 app.listen(PORT, () => {
   console.log('API server now on port ${PORT}!')
